09847dee452d6982b8da6ab1d86490d99751d3d6
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-29 21:06:29 -0600

    Add img to each book

--- src/book/book.js
+++ src/book/book.js
@@ -3,8 +3,14 @@ import "./book.css";
 
 class Book extends Component {
   render() {
+    // const test =
+    //   "http://books.google.com/books/content?id=XZAMHDbMtwIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
     return (
       <div className="book">
+        <img
+          className="thumbnail"
+          src={this.props.volumeInfo.imageLinks.thumbnail}
+        />
         <h2 className="title">{this.props.volumeInfo.title}</h2>
         <h4 className="authors">{this.props.volumeInfo.authors}</h4>
         <p className="description">{this.props.volumeInfo.description}</p>
@@ -14,3 +20,4 @@ class Book extends Component {
 }
 
 export default Book;
+// [""0""].volumeInfo.imageLinks.thumbnail
